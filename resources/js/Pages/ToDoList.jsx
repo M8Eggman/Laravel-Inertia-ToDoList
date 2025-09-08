9c6f589c50b2d2c62c33ccaff834126f24474e4d
GoNEAR <cagatay.morgul@outlook.com>
2025-09-08 14:17:11 +0200

    initialisation du projet

---
+++ resources/js/Pages/ToDoList.jsx
@@ -0,0 +1,6 @@
+
+export default function ToDoList() {
+  return (
+    <div>ToDoList</div>
+  )
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,25 @@
+import "./bootstrap";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { InertiaProgress } from "@inertiajs/progress";
+import { Ziggy, route } from 'ziggy-js';
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx");
+        return pages[`./Pages/${name}.jsx`]();
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+    title: (title) => `${title} - ${import.meta.env.VITE_APP_NAME || 'Laravel'}`,
+});
+
+InertiaProgress.init({
+    delay: 250,
+    color: "#4B5563",
+    includeCSS: true,
+    showSpinner: true,
+});
+
+window.route = (name, params, absolute) => route(name, params, absolute, Ziggy);
